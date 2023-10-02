9d02ef714c57b0ff734e5fe54247f77e04f22a3f
Raquel Brito <raquelbrito662@gmail.com>
2023-10-01 22:23:07 -0300

    update restcontroller

---
+++ src/main/java/com/imd/web/swaptales/controller/BookRestController.java
@@ -0,0 +1,10 @@
+package com.imd.web.swaptales.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/book")
+public class BookRestController {
+
+}
--- src/main/java/com/imd/web/swaptales/controller/ReviewRestController.java
+++ src/main/java/com/imd/web/swaptales/controller/ReviewRestController.java
@@ -3,10 +3,14 @@ package com.imd.web.swaptales.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.imd.web.swaptales.model.Review;
@@ -14,13 +18,19 @@ import com.imd.web.swaptales.service.ReviewService;
 
 @RestController
 @RequestMapping("/api/review")
-public class ReviewController {
+public class ReviewRestController {
 
 	private final ReviewService reviewService;	
 	
-	public ReviewController(ReviewService reviewService) {
+	public ReviewRestController(ReviewService reviewService) {
 		super();
 		this.reviewService = reviewService;
+	}
+
+	@PostMapping(value = "/new")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Review create(@RequestBody Review review) {
+		return reviewService.create(review);
 	}
 
 	@GetMapping(value = "/list")
