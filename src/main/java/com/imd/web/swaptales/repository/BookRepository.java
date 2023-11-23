ec33c791be84dba851f5ffc1229ee375374277cc
Raquel Brito <raquelbrito662@gmail.com>
2023-11-23 09:36:02 -0300

    best book

--- src/main/java/com/imd/web/swaptales/repository/BookRepository.java
+++ src/main/java/com/imd/web/swaptales/repository/BookRepository.java
@@ -15,7 +15,9 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByUserId(@Param("userId") Long userId);
 
     List<Book> findByAvailabilityStatus(@Param("availabilityStatus")AvailabilityStatus availabilityStatus);
-    
+
+    @Query("SELECT b FROM Book b LEFT JOIN FETCH b.reviews r ORDER BY AVG(r.stars) DESC, COUNT(r) DESC, MAX(r.reviewDate) DESC")
+    List<Book> listBestReview();
     //Query("SELECT b FROM Book b ORDER BY b.reviews.size DESC")
     //List<Book> findAllOrderedByReviewCount();
 }
