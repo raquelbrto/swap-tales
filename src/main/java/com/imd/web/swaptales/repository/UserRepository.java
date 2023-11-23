f800bb918e7981dba8bd12a8ac240bc9c70dcd97
Raquel Brito <raquelbrito662@gmail.com>
2023-11-23 09:54:02 -0300

    best user

--- src/main/java/com/imd/web/swaptales/repository/UserRepository.java
+++ src/main/java/com/imd/web/swaptales/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.imd.web.swaptales.repository;
 
 import com.imd.web.swaptales.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +16,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmailAndPassword(String email, String password);
+
+    @Query("SELECT u FROM User u " +
+            "LEFT JOIN FETCH u.followers f " +
+            "LEFT JOIN FETCH u.likedReviews r " +
+            "GROUP BY u " +
+            "ORDER BY COUNT(f) DESC, SUM(r.likesCount) DESC, COUNT(r) DESC")
+    List<User> listBestUser();
 }
